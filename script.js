f370b8f0263af44488695e282941e5b0ec8f77d9
Gabriel Sotto <gabrielsotto90@gmail.com>
2022-08-19 15:52:22 -0300

    add category filters

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 //DARK MODE
 
-// let darkMode = document.getElementById("darkMode");
-// let body = document.getElementById("body");
+let darkMode = document.getElementById("darkMode");
+let body = document.getElementById("body");
 
-// function cambiarModo() {
-//   if (body.classList.contains("dark")) {
-//     darkMode.innerText = "Dark";
-//     darkMode.classList.remove("btn-light");
-//     darkMode.classList.add("btn-dark");
-//   } else {
-//     darkMode.innerText = "Light";
-//     darkMode.classList.remove("btn-dark");
-//     darkMode.classList.add("btn-light");
-//   }
-//   body.classList.toggle("dark");
-// }
+darkMode.addEventListener("click", cambiarModo);
+
+function cambiarModo() {
+  if (body.classList.contains("dark")) {
+    darkMode.innerText = "Dark";
+    darkMode.classList.remove("btn-light");
+    darkMode.classList.add("btn-dark");
+  } else {
+    darkMode.innerText = "Light";
+    darkMode.classList.remove("btn-dark");
+    darkMode.classList.add("btn-light");
+  }
+  body.classList.toggle("dark");
+}
 
 //PRODUCTS
 
@@ -43,13 +45,16 @@ getProduct();
 let cards = document.getElementById("cardsContainer");
 
 function showProducts(products) {
+
+  cards.innerHTML = "";
+
   products.forEach(element => {
     cards.innerHTML += `
     <div class="col">
       <div class="card h-100">
-         <img src="${element.image}" class="card-img-top" alt="${element.description}">
+        <img src="${element.image}" class="card-img-top" alt="${element.description}">
         <div class="card-body">
-        <h3>${element.price}$</h3>
+          <h3>${element.price}$</h3>
           <h4 class="card-title">${element.title}</h4>
           <button class="btn btn-primary botonCompra">Agregar al carrito</button>
         </div>
@@ -98,25 +103,27 @@ function addCart(e) {
 
 function mostrarCarrito() {
   console.log(carrito)
-  for (let producto of carrito)
-  {
+  for (let producto of carrito) {
     let fila = document.createElement("tr");
-
+    
     fila.innerHTML = `<td><img src="${producto.img}"></td>
-                        <td>${producto.precio}</td>
-                        <td>${producto.nombre}</td>
-                        <td><button class="btn-danger borrarElemento">Borrar</button></td>`;
+    <td>${producto.precio}</td>
+    <td>${producto.nombre}</td>
+    <td><button class="btn-danger borrarElemento">Borrar</button></td>`;
     let tabla = document.getElementById("tbody");
-
+    
     tabla.append(fila);
   }
-
+  
   let botonesBorrar = document.querySelectorAll(".borrarElemento");
-
+  
   for (let boton of botonesBorrar) {
     boton.addEventListener("click", borrarProducto);
   }
 }
+
+let icon = document.querySelector("#iconCart");
+icon.addEventListener("click", mostrarCarrito);
 
 function borrarProducto(e) {
   let abuelo = e.target.parentNode.parentNode;
@@ -130,21 +137,60 @@ function borrarProducto(e) {
   }).showToast();
 }
 
-
-let icon=document.querySelector("#iconCart");
-icon.addEventListener("click", mostrarCarrito);
-
-
-
 //SEARCH FILTERS
 let search = document.querySelector("#input");
 
-search.addEventListener("keyup", (e)=>{
+search.addEventListener("keyup", (e) => {
 
   const input = e.target.value.toLowerCase()
 
-  let productFilter = products.filter(product => product.title.toLowerCase().includes(input))||(product => product.category.toLowerCase().includes(input));
+  let productFilter = products.filter(product => product.title.toLowerCase().includes(input)) || (product => product.category.toLowerCase().includes(input));
 
   console.log(productFilter);
+  console.log("input", input);
 
+  showProducts(productFilter);
 })
+
+
+// CATEGORY FILTERS
+
+let women = document.querySelector(".women");
+women.addEventListener("click", (e) => {
+  e.preventDefault()
+
+  let productFilter = products.filter(product => product.category === "women's clothing")
+
+  showProducts(productFilter);
+}
+)
+
+let mens = document.querySelector(".men");
+mens.addEventListener("click", (e) => {
+  e.preventDefault()
+
+  let productFilter = products.filter(product => product.category === "men's clothing")
+
+  showProducts(productFilter);
+}
+)
+
+let tech = document.querySelector(".tech");
+tech.addEventListener("click", (e) => {
+  e.preventDefault()
+
+  let productFilter = products.filter(product => product.category === "electronics")
+
+  showProducts(productFilter);
+}
+)
+
+let jewelery = document.querySelector(".jewelery");
+jewelery.addEventListener("click", (e) => {
+  e.preventDefault()
+
+  let productFilter = products.filter(product => product.category === "jewelery")
+
+  showProducts(productFilter);
+}
+)
